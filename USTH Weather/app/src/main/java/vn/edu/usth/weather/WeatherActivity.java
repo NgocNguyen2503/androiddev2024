f9f596891b9ca6e7fdba859ad4aab8ad4e60ccd5
NgocNguyen <ngocnq.ba12-139@st.usth.edu.vn>
2024-08-27 22:15:28 +0700

    Practical 3

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -9,6 +9,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
@@ -16,11 +18,22 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ForecastFragment forecastFragment = new ForecastFragment();
+
+        // Begin the transaction
+        getSupportFragmentManager().beginTransaction().add(R.id.container, forecastFragment).commit();;;
+
+        // Add the fragment to the 'container' FrameLayout
+
+
+        // Commit the transaction
+
     }
 
     @Override
