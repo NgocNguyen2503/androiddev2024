84ddf1e18f205374550d5e60658970ba4a9c3282
NgocNguyen <ngocnq.ba12-139@st.usth.edu.vn>
2024-08-28 14:26:17 +0700

    Practical 3

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -9,26 +9,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main); //khoi tao man hinh
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ForecastFragment forecastFragment = new ForecastFragment();
-
-
-        getSupportFragmentManager().beginTransaction().add(R.id.container, forecastFragment).commit();
-
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
     }
 
     @Override
@@ -60,4 +55,8 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("weather", "Destroy");
     }
+
+
+
+
 }
