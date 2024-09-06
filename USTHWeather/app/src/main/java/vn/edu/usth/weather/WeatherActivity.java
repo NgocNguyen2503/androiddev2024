0d46dd04c185d8c5dad61e10507f4ec00248d060
NgocNguyen <ngocnq.ba12-139@st.usth.edu.vn>
2024-09-06 10:08:12 +0700

    Practical 6

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -21,9 +21,9 @@ public class WeatherActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.main, firstFragment).commit();
+//        ForecastFragment firstFragment = new ForecastFragment();
+//        getSupportFragmentManager().beginTransaction().add(
+//                R.id.main, firstFragment).commit();
     }
 
     @Override
